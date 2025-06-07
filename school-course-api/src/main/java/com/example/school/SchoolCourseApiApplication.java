6d9eaa2562740efe7cb04ea82720008d2626cebc
Tyler Pitcher <20trlp@queensu.ca>
2025-06-07 14:00:45 -0400

    Add course api

---
+++ school-course-api/src/main/java/com/example/school/SchoolCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolCourseApiApplication.class, args);
+	}
+
+}
