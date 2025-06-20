86628596babb467f8a179afdfc5dbb8f86f3a44f
Tyler Pitcher <20trlp@queensu.ca>
2025-06-19 20:52:38 -0400

    Add create student form

--- school-client/src/app/students/students.component.ts
+++ school-client/src/app/students/students.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Apollo, gql } from 'apollo-angular';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 const STUDENTS_QUERY = gql`
   query {
@@ -13,20 +14,36 @@ const STUDENTS_QUERY = gql`
   }
 `;
 
+const CREATE_STUDENT_MUTATION = gql`
+  mutation CreateStudent($firstName: String!, $lastName: String!, $age: Int!) {
+    createStudent(firstName: $firstName, lastName: $lastName, age: $age) {
+      id
+      firstName
+      lastName
+    }
+  }
+`;
+
 @Component({
   selector: 'app-students',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './students.component.html',
 })
 export class StudentsComponent implements OnInit {
   students: any[] = [];
   error: any;
+  firstName = '';
+  lastName = '';
+  age: number | null = null;
+  loading = false;
 
-  constructor(private apollo: Apollo) {
-
-  }
+  constructor(private apollo: Apollo) {}
 
   ngOnInit(): void {
+    this.loadStudents();
+  }
+
+  loadStudents(): void {
     this.apollo.watchQuery({ query: STUDENTS_QUERY })
       .valueChanges.subscribe(({ data, error }: any) => {
         this.students = data.students;
@@ -34,4 +51,35 @@ export class StudentsComponent implements OnInit {
       });
   }
 
+  createStudent(): void {
+    if (!this.firstName || !this.lastName || this.age == null) {
+      this.error = 'All fields are required.';
+      return;
+    }
+
+    this.loading = true;
+    this.apollo.mutate({
+      mutation: CREATE_STUDENT_MUTATION,
+      variables: {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        age: this.age,
+      },
+    }).subscribe({
+      next: ({ data }: any) => {
+        const createdStudent = (data as any).createStudent;
+        this.students.push(createdStudent);
+        this.firstName = '';
+        this.lastName = '';
+        this.age = null;
+        this.error = null;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = err.message;
+        this.loading = false;
+      }
+    });
+  }
 }
+
